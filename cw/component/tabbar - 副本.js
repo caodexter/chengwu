1c8915629132b58e0421bce4fb8b0ade0d475b6e
unknown <caoxiaodong@nuctech.com>
2018-02-01 21:56:31 +0800

    Signed-off-by: unknown <caoxiaodong@nuctech.com>

--- cw/component/tabbar - 副本.js
+++ cw/component/tabbar - 副本.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { View, Text ,StyleSheet} from 'react-native'
 import Tabs from 'react-native-tabs'
-//import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons'
 import styles from '../config/style';
 
 export default class tabbar extends Component {
@@ -26,19 +26,19 @@ export default class tabbar extends Component {
     return (
       <Tabs selected={this.state.page} style={{ backgroundColor: 'white' }} selectedStyle={{ color: '#4F8EF7' }} onSelect={this.onSelect.bind(this)}>
         <Text name='home' style={styles.textAlignCenter} selectedIconStyle={styles.selectedTabStyle}>
-          列表
+          <Icon name='md-list' size={30} style={styles.iconStyle} />{'\n'}列表
         </Text>
         <Text name='model' style={styles.textAlignCenter} selectedIconStyle={styles.selectedTabStyle}>
-          图形
+          <Icon name='md-analytics' size={30} style={styles.iconStyle} />{'\n'}图形
         </Text>
         <Text name='mirror' style={styles.textAlignCenter} selectedIconStyle={styles.selectedTabStyle}>
-          分析
+          <Icon name='md-podium' size={30} style={styles.iconStyle} />{'\n'}分析
         </Text>
         <Text name='favorite' style={styles.textAlignCenter} selectedIconStyle={styles.selectedTabStyle}>
-          收藏
+          <Icon name='md-heart' size={30} style={styles.iconStyle} />{'\n'}收藏
         </Text>
         <Text name='me' style={styles.textAlignCenter} selectedIconStyle={styles.selectedTabStyle}>
-          我的
+          <Icon name='md-person' size={30} style={styles.iconStyle} />{'\n'}我
         </Text>
       </Tabs>
     )
